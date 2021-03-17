80f210edbaa5f882089b6cb9f7fadf68b6f8fa56
GhermanCristian <gherman_cristi10@yahoo.com>
2021-03-17 19:26:44 +0200

    Lab 2 - The GUI now displays the path and moves the drone accordingly

--- Lab2/GUI.py
+++ Lab2/GUI.py
@@ -4,6 +4,7 @@ from service import Service
 
 class GUI:
     def __init__(self):
+        self.__initPygame()
         self.__screen = pygame.display.set_mode((400,400))
         self.__screen.fill(Constants.WHITE)
         self.__service = Service()
@@ -13,14 +14,6 @@ class GUI:
         logo = pygame.image.load("logo32x32.png")
         pygame.display.set_icon(logo)
         pygame.display.set_caption("Doru Exploratoru revine")
-    
-    def __displayWithPath(self, image, path):
-        mark = pygame.Surface((20,20))
-        mark.fill(Constants.GREEN)
-        for move in path:
-            image.blit(mark, (move[1] *20, move[0] * 20))
-            
-        return image
     
     def __getMapImage(self, colour = Constants.BLUE, background = Constants.WHITE):
         image = pygame.Surface((400,400))
@@ -36,24 +29,35 @@ class GUI:
                 
         return image
     
+    def __displayWithPath(self, path):
+        droneImage = pygame.image.load("minune.jpg")
+        
+        visitedTile = pygame.Surface((20,20))
+        visitedTile.fill(Constants.GREEN)
+        
+        image = self.__getMapImage()
+        pathImage = self.__getMapImage()
+        
+        for move in path:
+            pathImage.blit(visitedTile, (move[1] *20, move[0] * 20))
+            pathImageCopy = pathImage.copy()
+            pathImageCopy.blit(droneImage, (move[1] *20, move[0] * 20))
+            self.__screen.blit(pathImageCopy, (0, 0))
+            pygame.display.update()
+            pygame.time.wait(400)
+            
+        return image
+    
     def __drawDrone(self, mapImage):
         droneImage = pygame.image.load("minune.jpg")
         mapImage.blit(droneImage, (self.__service.getDroneYCoord() * 20, self.__service.getDroneXCoord() * 20))
         return mapImage
     
     def start(self):
-        # define a variable to control the main loop
-        running = True
-         
-        # main loop
-        while running:
-            for event in pygame.event.get(): # event handling, gets all events from the event queue
-                if event.type == pygame.QUIT:
-                    running = False # exit the main loop
-            
-            pygame.time.wait(1)
-            self.__screen.blit(self.__drawDrone(self.__getMapImage()), (0, 0))
-            pygame.display.update()
+        self.__screen.blit(self.__drawDrone(self.__getMapImage()), (0, 0))
+        pygame.display.update()
         
-        pygame.display.flip()
+        self.__displayWithPath(self.__service.searchGreedy(2, 3, 19, 3))
+        pygame.display.update()
+        pygame.time.wait(5000)
         pygame.quit()
