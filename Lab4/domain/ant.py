2393d9a52e9ad436daacb69db599ac40453e2b27
GhermanCristian <gherman_cristi10@yahoo.com>
2021-04-03 16:50:31 +0300

    Lab 4 - First version of the Ant entity

---
+++ Lab4/domain/ant.py
@@ -0,0 +1,46 @@
+from constants import Constants
+import random
+
+
+class Ant:
+    def __init__(self):
+        self.__size = Constants.SENSOR_COUNT  # size of the path through all the sensors
+        self.__path = []  # will store the sensor indices
+        self.__path.append(random.randint(0, Constants.SENSOR_COUNT - 1))  # place it randomly on a sensor
+
+    def __getPossibleMoves(self):
+        possibleMoves = []
+        for sensorIndex in range(Constants.SENSOR_COUNT):
+            if sensorIndex not in self.__path:
+                possibleMoves.append(sensorIndex)
+        return possibleMoves
+
+    def __computeProbabilityOfChoosingNextSensor(self, possibleMoves, alpha, beta, distanceTable, pheromoneTable):
+        currentSensorIndex = self.__path[-1]
+        nextSensorProbability = [0 for i in range(self.__size)]
+
+        for moveIndex in range(len(possibleMoves)):
+            distanceToNextSensor = distanceTable[currentSensorIndex][moveIndex]
+            pheromoneToNextSensor = pheromoneTable[currentSensorIndex][moveIndex]
+            probability = (distanceToNextSensor ** beta) * (pheromoneToNextSensor ** alpha)
+            nextSensorProbability.append(probability)
+
+        return nextSensorProbability
+
+    def nextMove(self, distanceTable, pheromoneTable, q0, alpha, beta):
+        # q0 = probability that the ant chooses the best possible move; otherwise, all moves have a prob of being chosen
+        possibleMoves = self.__getPossibleMoves()
+        if not possibleMoves:
+            return False
+
+        nextSensorProbability = self.__computeProbabilityOfChoosingNextSensor(possibleMoves, alpha, beta, distanceTable, pheromoneTable)
+        if random.random() < q0:
+            bestProbability = max(nextSensorProbability)
+            self.__path.append(nextSensorProbability.index(bestProbability))
+        else:
+            # all moves have a prob of being chosen (roulettte)
+            # TO-DO
+            pass
+
+    def fitness(self):
+        return self.__size
