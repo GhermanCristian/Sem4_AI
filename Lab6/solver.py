a0ab3158c54050b0b7df5878326ec23cf196ebdb
GhermanCristian <gherman_cristi10@yahoo.com>
2021-04-25 10:21:49 +0300

    Lab 6 - Can now generate the initial centroids (randomly for now; perhaps one in each quadrant in the future)

--- Lab6/solver.py
+++ Lab6/solver.py
@@ -1,6 +1,12 @@
+import random
+
+
 class Solver:
+    CLUSTER_COUNT = 4
+
     def __init__(self):
         self.__pointList = []  # a point is of the form (label, val1, val2)
+        self.__centroids = []
 
     def __parseLineAndAddToList(self, line):
         splitData = line.split(",")
@@ -12,8 +18,11 @@ class Solver:
         for line in fileDesc:
             self.__parseLineAndAddToList(line[:-1])  # remove the trailing '\n'
 
+    def __generateInitialCentroids(self):
+        for _ in range(Solver.CLUSTER_COUNT):
+            self.__centroids.append((random.uniform(-9.99, 9.99), random.uniform(-9.99, 9.99)))
+
     def solve(self):
         self.__parseDataset()
-        for point in self.__pointList:
-            print(point)
+        self.__generateInitialCentroids()
 
