1d17ebc7e091c07f0b44a6e8c0c2f37c35136d03
GhermanCristian <gherman_cristi10@yahoo.com>
2021-04-27 10:11:36 +0300

    Lab 6 - Some refactoring in statistics

--- Lab6/statistics.py
+++ Lab6/statistics.py
@@ -4,6 +4,15 @@ from constants import Constants
 class Statistics:
     def __init__(self, pointList):
         self.__pointList = pointList
+        self.__computeConfusionMatrix()
+
+    def __computeConfusionMatrix(self):
+        self.__confusionMatrix = [[0 for _ in range(Constants.CLUSTER_COUNT)] for _ in range(Constants.CLUSTER_COUNT)]
+        labelToIndex = self.__rearrangeClusters()
+        for point in self.__pointList:
+            trueLabel = labelToIndex[point[2]]
+            predictedLabel = point[3]
+            self.__confusionMatrix[predictedLabel][trueLabel] += 1
 
     def __findOccurrencesInEachCluster(self, trueLabel):
         occurrences = [0 for _ in range(Constants.CLUSTER_COUNT)]
@@ -13,6 +22,10 @@ class Statistics:
         return occurrences
 
     def __rearrangeClusters(self):
+        """
+        We might end up with situations in which the clusters are correct (ex. all Bs in the same cluster etc.), but in the wrong order
+        (intuitively, the cluster order should be A B C D, but that rarely happens, and we end up with B D A C for example)
+        """
         labelToIndex = {"A": None, "B": None, "C": None, "D": None}
 
         for trueLabel in labelToIndex.keys():
@@ -28,17 +41,7 @@ class Statistics:
 
         return labelToIndex
 
-    def __computeConfusionMatrix(self):
-        confusionMatrix = [[0 for _ in range(Constants.CLUSTER_COUNT)] for _ in range(Constants.CLUSTER_COUNT)]
-        labelToIndex = self.__rearrangeClusters()
-        for point in self.__pointList:
-            trueLabel = labelToIndex[point[2]]
-            predictedLabel = point[3]
-            confusionMatrix[predictedLabel][trueLabel] += 1
-
-        return confusionMatrix
-
-    def __computeMeasurementsSpecificLabel(self, labelIndex, confusionMatrix):
+    def __computeMeasurementsSpecificLabel(self, labelIndex):
         """
         for each label, i = label's index:
             true positive = m[i][i]
@@ -46,25 +49,24 @@ class Statistics:
             false positive = sum(m[i][j]), j != i -> just the current row (without position [i][i])
             false negative = sum(m[j][i]), j != i -> just the current column (without position [i][i])
         """
-        truePositive = confusionMatrix[labelIndex][labelIndex]
+        truePositive = self.__confusionMatrix[labelIndex][labelIndex]
 
         trueNegative, falsePositive, falseNegative = 0, 0, 0
         for i in range(Constants.CLUSTER_COUNT):
             for j in range(Constants.CLUSTER_COUNT):
                 if i != labelIndex and j != labelIndex:
-                    trueNegative += confusionMatrix[i][j]
+                    trueNegative += self.__confusionMatrix[i][j]
                 elif i == labelIndex and j != labelIndex:
-                    falsePositive += confusionMatrix[i][j]
+                    falsePositive += self.__confusionMatrix[i][j]
                 elif i != labelIndex and j == labelIndex:
-                    falseNegative += confusionMatrix[i][j]
+                    falseNegative += self.__confusionMatrix[i][j]
 
         return truePositive, trueNegative, falsePositive, falseNegative
 
     def computeMeasurements(self):
-        confusionMatrix = self.__computeConfusionMatrix()
         results = []  # rows: 0 = accuracy, 1 = precision, 2 = rappel, 3 = score
         for labelIndex in range(Constants.CLUSTER_COUNT):
-            truePositive, trueNegative, falsePositive, falseNegative = self.__computeMeasurementsSpecificLabel(labelIndex, confusionMatrix)
+            truePositive, trueNegative, falsePositive, falseNegative = self.__computeMeasurementsSpecificLabel(labelIndex)
             accuracy = (truePositive + trueNegative) / (truePositive + trueNegative + falsePositive + falseNegative)
             precision = truePositive / (truePositive + falsePositive)
             rappel = truePositive / (truePositive + falseNegative)
@@ -81,4 +83,4 @@ class Statistics:
         print("Average precision: ", (sum(row[1] for row in results)) / Constants.CLUSTER_COUNT)
         print("Average rappel: ", (sum(row[2] for row in results)) / Constants.CLUSTER_COUNT)
         print("Average score: ", (sum(row[3] for row in results)) / Constants.CLUSTER_COUNT)
-        print(confusionMatrix)
+        print(self.__confusionMatrix)
