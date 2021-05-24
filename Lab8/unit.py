8db2d2fdefea0712e7f89e83bd6f803188abae46
GhermanCristian <gherman_cristi10@yahoo.com>
2021-05-24 17:12:08 +0300

    Lab 8 - Added the Unit class

---
+++ Lab8/unit.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+
+class Unit(nn.Module):
+    def __init__(self, inChannels, outChannels):
+        super(Unit, self).__init__()
+
+        self.conv = nn.Conv2d(in_channels=inChannels, kernel_size=3, out_channels=outChannels, stride=1, padding=1)
+        self.bn = nn.BatchNorm2d(numFeatures=outChannels)
+        self.relu = nn.ReLU()
+
+    def forward(self, inputData):
+        return self.relu(self.bn(self.conv(inputData)))
